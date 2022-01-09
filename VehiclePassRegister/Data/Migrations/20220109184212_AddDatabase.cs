db9887b46a0c54e5b5e7d9f93961c18f5b909d03
sagor31h2 <sagor31h2@gmail.com>
2022-01-10 00:46:45 +0600

    First Migration

---
+++ VehiclePassRegister/Data/Migrations/20220109184212_AddDatabase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VehiclePassRegister.Data.Migrations
+{
+    public partial class AddDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Vehicles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    VechicleNo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DriverName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DriverPhoneNo = table.Column<int>(type: "int", nullable: false),
+                    PassingTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vehicles", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Vehicles");
+        }
+    }
+}
